a47afd060a5b25c38bad30cba4b8db8ea73b1414
ArtemSid <artemsidorchik@gmail.com>
2019-09-17 17:30:11 +0300

    Stage 5
    Raycast

---
+++ Assets/Scripts/Quests/Quest001Take.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Quest001Take : MonoBehaviour
+{
+    public float theDistance;
+
+    public GameObject actionDisplay;
+    public GameObject actionText;
+    public GameObject UIQuest;
+    public GameObject thePlayer;
+    public GameObject noticeCam;
+
+    void Update()
+    {
+        theDistance = PlayerCasting.distanceFromTarget;
+    }
+
+    void OnMouseOver()
+    {
+        if (theDistance <= 3)
+        {
+            actionDisplay.SetActive(true);
+            actionText.SetActive(true);
+        }
+
+        if (Input.GetButtonDown("Action"))
+        {
+            if (theDistance <= 3)
+            {
+                actionDisplay.SetActive(false);
+                actionText.SetActive(false);
+                UIQuest.SetActive(true);
+                noticeCam.SetActive(true);
+                thePlayer.SetActive(false);
+            }
+        }
+    }
+
+    void OnMouseExit()
+    {
+        actionDisplay.SetActive(false);
+        actionText.SetActive(false);
+    }
+}
