7370ece459c010fd12da7a92a57a9c1cc3b85590
ArtemSid <artemsidorchik@gmail.com>
2019-09-26 16:21:38 +0300

    Stage 12
    Enemies

---
+++ Assets/Scripts/EnemyScripts/InfictDamage.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class InfictDamage : MonoBehaviour
+{
+    public int damageAmout = 5;
+    public float targetDistance;
+    public float allowedRange = 2.7f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+            {
+                targetDistance = hit.distance;
+
+                if (targetDistance <= allowedRange)
+                {
+                    hit.transform.SendMessage("DeductPoints", damageAmout, SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+    }
+}
